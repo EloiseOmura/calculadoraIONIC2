7668d67949678c16066504befceddf1b2ad03cb1
Eloíse <eloiseomura@alunos.utfpr.edu.br>
2023-06-13 22:19:49 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,60 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public numero ="0"
+  public numeroAux ="0"
+
   constructor() {}
+
+  ngOnInit(){
+  this.numero = "0"; 
+  this.numeroAux = "0";
+ }
+
+ somar(){}
+
+ sub(){}
+
+ mult(){}
+
+ dividir(){}
+
+ clear(){
+this.numero = "0";
+this.numeroAux = "0";
+ }
+
+ negativo(){
+  if(this.numero != '0') {
+    this.numero = this.numero.includes('-') 
+      ? this.numero.replace('-', "")
+      : '-' + this.numero;
+  }
+ 
+ }
+
+ percent(){
+
+ }
+
+ igual(){
+
+ }
+
+ numeros(num:string) {
+  if(this.numero.startsWith('0')){
+   this.numero = "" 
+  }
+  if(this.numero.length <=8) {
+    this.numero += num;
+  }
+  
+ }
+
+ virgula() {
+  if(this.numero.length <=8 && !this.numero.includes(',')) {
+    this.numero += ',';
+  }
+ }
 
 }
